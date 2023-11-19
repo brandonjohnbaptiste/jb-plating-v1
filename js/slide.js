29e0e7db0bcf1afcb49feda4bc8a6bfc38b8cf90
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2023-11-19 16:22:43 +0000

    image slider with next and previous buttons

--- js/slide.js
+++ js/slide.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', evt => {
-    let currSlide = 0;
+
     const nextBtn = document.querySelector('.btn-next');
+    const prevBtn = document.querySelector('.btn-prev')
     const slides = document.querySelectorAll('.slide');
+
+    let currSlide = 0;
+    let maxSlide = slides.length - 1;
 
     console.log(nextBtn);
     nextBtn.addEventListener('click', function() {
-       currSlide++;
+       if (currSlide === maxSlide) return;
+        currSlide++;
+
 
        slides.forEach((slide, index) => {
           slide.style.transform = `translateX(${100 * (index - currSlide)}%)`;
        });
+    });
+
+    prevBtn.addEventListener('click', function() {
+        if (currSlide === 0) return;
+        currSlide--;
+
+
+        slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${100 * (index - currSlide)}%)`;
+        });
     });
 });
 
