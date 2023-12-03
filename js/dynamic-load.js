9f744e2473782acd2abd436fef06c63cd7f7224a
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2023-12-03 14:19:53 +0000

    added contact page outline

--- js/dynamic-load.js
+++
@@ -1,8 +0,0 @@
-const content = document.querySelector('.section');
-
-const options = {};
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-       console.log(entry);
-    });
-}, options);
